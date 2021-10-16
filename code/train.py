ac9f5069f23041881e4c5391a022d8caf8ce3749
obiii <obaidurrehman1994@gmail.com>
2021-10-16 22:39:50 +0200

    Workflow

--- code/train.py
+++ code/train.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import json
 from joblib import dump, load
+import pickle
 
 from sklearn.linear_model import LogisticRegression, RidgeClassifierCV
 from sklearn.svm import SVC, LinearSVC
